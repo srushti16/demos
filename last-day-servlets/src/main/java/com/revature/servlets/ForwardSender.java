2f24d17d157a07c6139730e6c69e27ea1b311755
Blake Kruppa <btkruppa513@gmail.com>
2018-02-20 09:42:22 -0500

    added forward example

---
+++ last-day-servlets/src/main/java/com/revature/servlets/ForwardSender.java
@@ -0,0 +1,24 @@
+package com.revature.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.catalina.servlets.DefaultServlet;
+import org.apache.log4j.Logger;
+
+public class ForwardSender extends DefaultServlet {
+	private Logger log = Logger.getRootLogger();
+	
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		log.trace("request received by ForwardSender, forwarding to the receiver");
+		request.getRequestDispatcher("/receiver").forward(request, response);
+		
+	}
+
+}
